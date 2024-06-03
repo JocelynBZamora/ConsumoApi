bfa1fd9dac2214e94107b2617ed38c9dd740bd29
Jocelyn Berenice Zamora De La Cerda <201G0293@rcarbonifera.tecnm.mx>
2024-06-03 15:16:22 -0600

    update

--- src/app/edit-product/edit-product.component.ts
+++ src/app/edit-product/edit-product.component.ts
@@ -1,33 +1,64 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MyFakeStoreServiceService } from '../my-fake-store-service.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-product',
   templateUrl: './edit-product.component.html',
-  styleUrl: './edit-product.component.css'
+  styleUrls: ['./edit-product.component.css'] // Corregido a styleUrls
 })
 export class EditProductComponent implements OnInit {
-  product: any;
+  categorias: any;
+  productForm: FormGroup;
 
   constructor(
+    private fb: FormBuilder,
     private route: ActivatedRoute,
     private fakestoreService: MyFakeStoreServiceService,
     private router: Router
-  ) { }
+  ) {
+    this.productForm = this.fb.group({
+      title: ['', Validators.required],
+      price: [0, Validators.required],
+      description: ['', Validators.required],
+      image: ['', [Validators.required, Validators.pattern(/https?:\/\/.+\.(jpg|jpeg|png)/)]], // Validar URL de imagen
+      categoryId: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
     const id = +this.route.snapshot.paramMap.get('id')!;
     this.fakestoreService.getProducto(id).subscribe(data => {
-      this.product = data;
+      // Asignamos los valores recibidos del servicio a los controles del formulario
+      this.productForm.patchValue({
+        title: data.title,
+        price: data.price,
+        description: data.description,
+        image: data.image,
+        categoryId: data.categoryId
+      });
+      console.log(data);
     });
+    this.categorias = JSON.parse(sessionStorage.getItem("categorias") ?? '[]');
   }
 
   updateProduct(): void {
-    const id = +this.route.snapshot.paramMap.get('id')!;
-    this.fakestoreService.updateProducto(id, this.product).subscribe(data => {
-      this.router.navigate(['/products']);
-      console.log(data);
-  });
-}
+    if(this.productForm.valid){
+      const productData = {
+        ...this.productForm.value,
+        images: [this.productForm.value.image] // Convertimos la URL en un array
+      };
+      console.log(productData);
+      const id = +this.route.snapshot.paramMap.get('id')!;
+      this.fakestoreService.updateProducto(id, productData).subscribe(data => { // Enviamos productData
+          console.log(data);
+          this.router.navigate(['/products']);
+        },
+        error => {
+          console.error('Error al crear el producto:', error);
+        }
+      );
+    }
+  }
 }
